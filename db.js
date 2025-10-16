0d7cb6902742eb7c82baaf2ff8a1a3f4cb27215b
Ethan7862 <kusasirakwe.ethan.upti@gmail.com>
2025-10-16 15:42:57 +0300

    dev

--- db.js
+++ db.js
@@ -4,8 +4,7 @@ require('dotenv').config();
 const connectionString = "postgresql://form_inputs_user:RG8kSXOIm6f4G0WdQrQzuvQJHAA6F7D9@dpg-d3oeen63jp1c73bu0ba0-a/form_inputs";
 
 if (!connectionString) {
-  console.error('Missing DATABASE_URL in environment. Exiting.');
-  process.exit(1);
+  console.error('Missing DATABASE_URL in environment. Exiting.')
 }
 
 const pool = new Pool({ connectionString });
